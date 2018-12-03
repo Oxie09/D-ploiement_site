7815ca1c14486fcf55f2367e6f38f2565016898f
FERRET <oceane.ferret@etu.uca.fr>
2018-12-03 12:16:38 +0100

    database

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 module.exports = {
-    database: '',
-    username: '',
-    password: '',
+    database: 'b2qdohunq',
+    username: 'u7wetinqpmp4r8i3',
+    password: 'RcGa2CCjZnN3VBkrtQJ',
     options: {
         host: 'localhost',
         dialect: 'mysql'
