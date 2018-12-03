5438c82ad6d96f4cb94719318256649452e38d8d
FERRET <oceane.ferret@etu.uca.fr>
2018-12-03 12:22:24 +0100

    modif host

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,9 @@
 module.exports = {
-    database: 'b2qdohunq',
-    username: 'u7wetinqpmp4r8i3',
-    password: 'RcGa2CCjZnN3VBkrtQJ',
+    database: process.env.MYSQL_ADDON_DB,
+    username: process.env.MYSQL_ADDON_USER,
+    password: process.env.MYSQL_ADDON_PASSWORD,
     options: {
-        host: 'localhost',
+        host: process.env.MYSQL_ADDON_HOST,
         dialect: 'mysql'
     }
 }
